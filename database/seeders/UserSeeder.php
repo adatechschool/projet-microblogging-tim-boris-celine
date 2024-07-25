2fd75764fd58f7a071d6e7ffd46e0ad0107554a5
Céline <celinesou77@gmail.com>
2024-07-25 09:31:48 +0000

    création seeder & factory pour users & posts

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post;
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class PostSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Post::factory()->count(10)->create();
+        User::factory()->count(10)->create();
     }
 }
