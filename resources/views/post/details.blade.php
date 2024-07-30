d44fef4814750d7852de587f85aa81e4eda29377
Terrassier Thimothée <thimotheeterrassier@gmail.com>
2024-07-30 09:57:28 +0200

    modif page detail post

--- resources/views/post/details.blade.php
+++ resources/views/post/details.blade.php
@@ -6,14 +6,14 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-12 lg:px-14">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="max-w-7xl mx-auto px-6 sm:px-12 lg:px-14">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                 <div class="flex place-content-between"><p>Post by : {{ $post->user->name }}</p> <p>{{ $post->created_at }}</p></div>
                 <br>
-                <h1>{{ $post->title }}</h1>
-                <br>
                 <div class="flex justify-center"><img src="{{ $post->image }}" class="h-[300px] w-auto rounded-xl" /></div>
+                <br>
+                <h1 class="sm:text-2xl font-semibold">{{ $post->title }}</h1>
                 <br>
                 <p>{{ $post->content }}</p>
                 </div>
