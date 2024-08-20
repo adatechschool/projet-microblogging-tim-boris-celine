ebcd32e703dbd35e5c72e05db9948a09b52ebd05
Céline <celinesou77@gmail.com>
2024-08-20 14:52:59 +0000

    plusieurs tests fonctionnelles

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -19,16 +19,42 @@ class ExampleTest extends TestCase
 
     public function test_wrong_password(): void
     {
-        $response = $this->post('/login', ['email' => 'truc@truc.truc', 'password' => 'machin']);
+        $response = $this->post('/login', ['email' => 'machin@machin.machin', 'password' => 'machin']);
 
         $response->assertStatus(302);
     }
 
     public function test_right_password(): void
     {
-        $response = $this->post('/login', ['email' => 'truc@truc.truc', 'password' => 'tructruc']);
+        $response = $this->post('/login', ['email' => 'machin@machin.machin', 'password' => 'machinmachin']);
 
         $response->assertStatus(302);
         $response->assertRedirectToRoute('dashboard');
     }
+
+    public function test_register_incorrect_form_email(): void
+    {
+        $response = $this->post('/register', ['name' => 'machin', 'email' => 'machinmachin.machin', 'password' => 'machinmachin']);
+        $response->assertSessionHasErrors('email');
+        $response->assertStatus(302);
+
+        $this->assertDatabaseMissing('users', ['name' => 'machin', 'email' => 'machinmachin.machin']);
+    }
+
+    public function test_email_already_exists(): void
+    {
+        $response = $this->post('/register', ['name' => 'machin', 'email' => 'machin@machin.machin', 'password' => 'machinmachin']);
+        $response->assertSessionHasErrors('email');
+        $response->assertStatus(302);
+
+        $this->assertDatabaseMissing('users', ['name' => 'machin', 'email' => 'machinmachin.machin']);
+    }
+
+    /* public function test_can_create_new_user(): void {
+        $response = $this->post('/register', ['name' => 'test', 'email' => 'test@test.test', 'password' => 'password']);
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('users', ['name' => 'test', 'email' => 'test@test.test']);
+    } */
+
 }
